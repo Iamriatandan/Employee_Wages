242c07e76a73ccab123fa490721134cff83d5389
iamriatandan <tandanria@gmail.com>
2025-03-05 15:49:44 +0530

    Ria Added . UC5 Working Days and Working hours

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//UC4 Calculating daily wage using function
+//UC5 Calculating daily wage using function days =20 or working hours = 160
 
 //function to calculate working hours
 
@@ -8,30 +8,34 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 
     switch(empCheck){
-        case 1 : empHours = PART_TIME_HOURS;
+        case 1 : totalEmpHours = PART_TIME_HOURS;
         console.log("Employee worked Part-Time");
         break;
         
-        case 2 : empHours = FULL_TIME_HOURS;
+        case 2 : totalEmpHours = FULL_TIME_HOURS;
         console.log("Employee worked Full-Time");
         break;
         
-        default : empHours =0;
+        default : totalEmpHours =0;
         console.log("Employee did not work");
         break;
         }
         
-        return empHours;
+        return totalEmpHours;
 }
 
 const WAGE_PER_HOUR = 20;
 
-let empHours =0;
+const MAX_HOURS_IN_A_MONTH =160;
 const NUM_OF_WORKING_DAYS = 20;
-for(let day = 0 ;day<NUM_OF_WORKING_DAYS;day++){
+let totalEmpHours =0;
+let totalWorkingDays =0;
+
+while(totalEmpHours<MAX_HOURS_IN_A_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random()*10) % 3; 
-    empHours += getWorkingHours(empCheck);
+    totalEmpHours += getWorkingHours(empCheck);
 }
 
-let empWage = empHours * WAGE_PER_HOUR;
-console.log("Total Hours : " , empHours , "Employee Wage : " , empWage);
+let empWage = totalEmpHours * WAGE_PER_HOUR;
+console.log( " UC5 :- Total Days : " , totalWorkingDays ,"Total Hours : " , totalEmpHours , "Employee Wage : " , empWage);
